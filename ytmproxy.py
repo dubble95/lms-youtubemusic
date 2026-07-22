d96310adaed605a7305f4f4c93094ed9b0d41fba
dubble <dubble95@gmail.com>
2026-07-22 13:59:11 +0900

    Use quickjs exclusively in ytmproxy.py for sub-second JS challenge solving

--- ytmproxy.py
+++ ytmproxy.py
@@ -887,8 +887,7 @@ def stream_audio(video_id):
         "--no-part",
         "-f", "bestaudio/best",
         "--js-runtimes", "quickjs",
-        "--js-runtimes", "node",
-        "--extractor-args", "youtube:player_client=android,web",
+        "--extractor-args", "youtube:player_client=mweb,ios,android,web",
         "-o", "-",
         url,
     ]
